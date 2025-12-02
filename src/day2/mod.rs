e77c0ea69e9ccf331ab16fcb7a578182d7191ded
Max Teegen <m_teegen@gmx.de>
2025-12-02 19:09:35 +0100

    day 2 part 2 solved

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -37,6 +37,58 @@ pub fn part1(puzzle: &str) -> Output {
     part_one(puzzle)
 }
 
+fn is_sillier_number(id: u64) -> bool {
+    let formatted = id.to_string();
+    let formatted = formatted.as_bytes();
+    let len = formatted.len();
+
+    for div in 1..=(len / 2) {
+        if len.is_multiple_of(div) {
+            let init = &formatted[..div];
+            let mut parts = formatted.chunks(div);
+            if parts.all(|p| p == init) {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+#[test]
+fn is_sillier_number_test() {
+    assert!(is_sillier_number(11));
+    assert!(is_sillier_number(22));
+    assert!(is_sillier_number(555));
+    assert!(is_sillier_number(1188511885));
+    assert!(is_sillier_number(2121212121));
+}
+
+#[aoc(day2, part2)]
+fn part_two(input: &str) -> Output {
+    let input = input.as_bytes();
+    let mut cursor = 0;
+
+    let mut id_sum = 0;
+
+    while cursor < input.len() {
+        let (range_start, start_digits) = parse_initial_digits_unsigned_u64(&input[cursor..]);
+        cursor += start_digits + 1;
+        let (range_end, end_digits) = parse_initial_digits_unsigned_u64(&input[cursor..]);
+        cursor += end_digits + 1;
+        for i in range_start..=range_end {
+            if is_sillier_number(i) {
+                id_sum += i;
+            }
+        }
+    }
+
+    id_sum
+}
+
+pub fn part2(puzzle: &str) -> Output {
+    part_two(puzzle)
+}
+
 #[cfg(test)]
 mod examples {
     use super::*;
@@ -45,5 +97,11 @@ mod examples {
     fn example1() {
         let res = part1(include_str!("test.txt"));
         assert_eq!(res, 1227775554);
+    }
+
+    #[test]
+    fn example2() {
+        let res = part2(include_str!("test.txt"));
+        assert_eq!(res, 4174379265);
     }
 }
