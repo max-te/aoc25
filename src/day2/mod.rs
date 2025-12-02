8e8c32296e6093576281c07368692991870c9190
Max Teegen <m_teegen@gmx.de>
2025-12-02 20:52:56 +0100

    opt(day2/part1): construct all the silly numbers in range instead of checking each number in range for silliness.
    We stop iterating over the whole range, and instead
    jump to next silly number immediately and then check if it is still in the range.
    
    The smallest silly number for a given even number of digits is 10^(digits) + 10^(digits/2).
    From there, all silly numbers are constructed by incrementing by 10^(digits/2 + 1) + 1.
    The rest is bookkeping of the current number of digits and range checks.

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -4,30 +4,6 @@ use crate::util::parse_initial_digits_unsigned_u64;
 
 type Output = u64;
 
-fn is_silly_number(id: u64, digits: u32) -> bool {
-    if digits.is_multiple_of(2) {
-        let divisor = 10u64.pow(digits / 2);
-        let left = id / divisor;
-        let expected = left * divisor + left;
-
-        id == expected
-    } else {
-        false
-    }
-}
-
-#[cfg(test)]
-#[test]
-fn is_silly_number_test() {
-    assert!(is_silly_number(11, 2));
-    assert!(is_silly_number(22, 2));
-    assert!(is_silly_number(1188511885, 10));
-    assert!(is_silly_number(21212121, 8));
-    assert!(!is_silly_number(555, 3));
-    assert!(!is_silly_number(1234, 4));
-    assert!(!is_silly_number(989876, 6));
-}
-
 #[aoc(day2, part1)]
 fn part_one(input: &str) -> Output {
     let input = input.as_bytes();
@@ -41,18 +17,42 @@ fn part_one(input: &str) -> Output {
         let (range_end, end_digits) = parse_initial_digits_unsigned_u64(&input[cursor..]);
         cursor += end_digits + 1;
 
-        let mut digits = start_digits as u32;
-        let mut digit_increment = 10u64.pow(digits);
+        let mut half_digits = (start_digits as u32).div_ceil(2);
 
-        for i in range_start..=range_end {
-            if i == digit_increment {
-                digit_increment *= 10;
-                digits += 1;
-                // Powers of 10 are not silly
-            } else if is_silly_number(i, digits) {
-                id_sum += i;
+        let mut silly_step = 10u64.pow(half_digits) + 1;
+        let mut silly = if !start_digits.is_multiple_of(2) {
+            10u64.pow(half_digits * 2 - 1) + 10u64.pow(half_digits - 1)
+        } else {
+            let divisor = 10u64.pow(half_digits);
+            let left = range_start / divisor;
+            let silly = left + left * divisor;
+            if silly >= range_start {
+                silly
+            } else {
+                silly + silly_step
+            }
+        };
+
+        let mut digit_increment = silly_step * 10u64.pow(half_digits);
+        // println!(
+        //     "Next silly from {range_start} is {silly} with {half_digits}*2 digits. Go in {silly_step}s until {digit_increment}"
+        // );
+        while silly <= range_end {
+            if silly == digit_increment {
+                half_digits += 1;
+                silly_step = 10u64.pow(half_digits) + 1;
+                silly = 10u64.pow(half_digits * 2 - 1) + 10u64.pow(half_digits - 1);
+                digit_increment = silly_step * 10u64.pow(half_digits);
+                // println!(
+                //     "Silly jump to {silly} with {half_digits}*2 digits. Go in {silly_step}s until {digit_increment}"
+                // );
+            } else {
+                id_sum += silly;
+                silly += silly_step;
+                // println!("Next {silly}")
             }
         }
+        // println!("reached end {range_end}");
     }
 
     id_sum
